75c87c11cfdcd269bcb02aafe2c7aec55651d6b6
Rafif-A-Zayed <rafif.zayed@gmail.com>
2025-11-02 20:13:36 +0400

    add in repo entity graph

--- src/main/java/com/drones/repository/DroneRepository.java
+++ src/main/java/com/drones/repository/DroneRepository.java
@@ -2,6 +2,8 @@ package com.drones.repository;
 
 import com.drones.enums.State;
 import com.drones.model.Drone;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
@@ -12,4 +14,7 @@ public interface DroneRepository extends CrudRepository<Drone,String> {
     List<Drone> getDroneByState(State state);
 
     List<Drone> findByCapacityLessThan(Double capacity);
+    @EntityGraph(attributePaths = "medications")
+    @Query("SELECT d FROM Drone d")
+    List<Drone> findAllWithMedications();
 }
